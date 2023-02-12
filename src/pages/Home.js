05f5768417188ed9af4af4d3dc1d7ee40fb9942f
Hamza <hmaqsood295@gmail.com>
2023-02-12 18:31:06 +0500

    final commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import HeroSection from "../components/HeroSection"
-import Benefits from "../components/Benefits"
-import Changes from "../components/ReadyForChange"
-import Prices from "../components/Prices"
-import Testimonials from "../components/Testimonials"
+
 
 const Home = () => {
   return (
     <>
-      <HeroSection />
-      <Benefits />
-      <Changes />
-      <Prices/>
-      <Testimonials/>
+     
     </>
   );
 }
